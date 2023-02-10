d46b6680a755fa80e82ac405b5b7244a4c948971
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-10 12:20:15 -0800

    Create merchant serializer

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,15 +9,24 @@ describe 'Merchants API' do
     expect(response).to be_successful
     merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(3)
+    expect(merchants).to be_a(Hash)
+    expect(merchants.keys.count).to eq 1
+    expect(merchants.keys[0]).to eq(:data)
+    expect(merchants[:data]).to be_a(Array)
+    expect(merchants[:data].count).to eq 3
+    expect(merchants[:data].first).to be_a(Hash)
 
-    merchants.each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+    expect(merchants[:data].first).to have_key(:id)
+    expect(merchants[:data].first[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
-    end
+    expect(merchants[:data].first).to have_key(:type)
+    expect(merchants[:data].first[:type]).to eq('merchant')
+
+    expect(merchants[:data].first).to have_key(:attributes)
+    expect(merchants[:data].first[:attributes]).to be_a(Hash)
+    
+    expect(merchants[:data].first[:attributes]).to have_key(:name)
+    expect(merchants[:data].first[:attributes][:name]).to be_a(String)
   end
 
   it 'sends a single merchant' do
